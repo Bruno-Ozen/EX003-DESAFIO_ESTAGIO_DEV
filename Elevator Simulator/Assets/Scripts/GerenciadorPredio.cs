b2b0eab82153bd45bbaff98e807c95b197eb1285
Bruno Henrique de Pinho <112650688+Bruno-Ozen@users.noreply.github.com>
2025-02-18 10:20:01 -0300

    Contrução das interfaces do simulador
    Interfaces e sprites feitos no Inkscape, sem recorrer a terceiros.

---
+++ Elevator Simulator/Assets/Scripts/GerenciadorPredio.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerenciadorPredio : MonoBehaviour
+{
+    //[SerializeField] private
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
